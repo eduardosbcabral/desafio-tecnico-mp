b1be52ccaac2b3c47f204f8cb390404b18d84a81
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-11-22 00:30:14 -0300

    Create BytesService

---
+++ DesafioTecnicoMP/BytesService.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace DesafioTecnicoMP
+{
+    public class BytesService
+    {
+        public static int CountFromString(string str)
+        {
+            return Encoding.UTF8.GetByteCount(str);
+        }
+    }
+}
