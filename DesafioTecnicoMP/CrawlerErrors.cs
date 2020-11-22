c62ae67302e92dff42151d682b14dfab46f6372e
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-11-22 00:29:48 -0300

    Create errors class

---
+++ DesafioTecnicoMP/CrawlerErrors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesafioTecnicoMP
+{
+    public class CrawlerErrors
+    {
+        private static string[] errors = new string[]
+        {
+            "Deu erro!",
+            "Erro na aplicação.",
+            "Ocorreu um erro ao executar a operação"
+        };
+
+        public static string Random()
+        {
+            var random = new Random();
+            return errors[random.Next(0, errors.Length)];
+        }
+    }
+}
