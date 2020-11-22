3841254b82d8e62a320037ef8adaf08853e3b7d1
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-11-22 00:30:58 -0300

    Implement crawler exercise steps

--- DesafioTecnicoMP/Program.cs
+++ DesafioTecnicoMP/Program.cs
@@ -6,7 +6,45 @@ namespace DesafioTecnicoMP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = @"https://lerolero.com/";
+            var url2 = @"https://mothereff.in/byte-counter#";
+
+            CrawlerService crawler = new CrawlerService(url);
+
+            var sentence = string.Empty;
+
+            try
+            {
+                sentence = crawler
+                    .GoToUrl(url)
+                    .GetTextContentFromElement(".sentence");
+            } 
+            catch(CrawlerException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var bytesCount = 0;
+
+            try
+            {
+                var bytesFromPage = crawler
+                    .GoToUrl(url2+sentence)
+                    .GetTextContentFromElement("#bytes");
+
+                bytesCount = int.Parse(bytesFromPage.Split(" ")[0]);
+            }
+            catch (CrawlerException ex)
+            {
+                bytesCount = BytesService.CountFromString(sentence); 
+            }
+            finally
+            {
+                crawler.Quit();
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
